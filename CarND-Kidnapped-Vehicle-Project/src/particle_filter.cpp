a839057a9cb578a2711b085029785b8e8ed1653e
Andres <andres@ubuntu.ubuntu-domain>
2020-03-27 13:51:17 -0600

    feat: Beta version of the project

--- CarND-Kidnapped-Vehicle-Project/src/particle_filter.cpp
+++ CarND-Kidnapped-Vehicle-Project/src/particle_filter.cpp
@@ -31,7 +31,8 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 1000;  // TODO: Set the number of particles
+  // The num_particles can be change to any value between 5-100
+  num_particles = 100;  // TODO: Set the number of particles
   // Create the variable for Randomness
   std::default_random_engine gen;
   // This line creates a normal (Gaussian) distribution for x
@@ -69,14 +70,15 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
   std::default_random_engine gen;
   // Variables to store the motion model estimated positions
   double x_f, y_f, theta_f; 
-      
+  //For every particle
   for(int i=0; i < num_particles; i++){
-    // According to the Bicycle motion model we have the following equations for the prediction (x,y,theta)
+    // Calcualte according to the Bicycle motion model we have the following equations for the prediction (x,y,theta)
     if(yaw_rate != 0){
       x_f = particles[i].x + (velocity/yaw_rate)*(sin(particles[i].theta + yaw_rate*delta_t) - sin(particles[i].theta));
       y_f = particles[i].y + (velocity/yaw_rate)*(cos(particles[i].theta) - cos(particles[i].theta + yaw_rate*delta_t));
       theta_f = particles[i].theta + yaw_rate*delta_t;
     }
+    //Avoid division by zero
     else{
       x_f = particles[i].x + velocity*delta_t*cos(particles[i].theta);
       y_f = particles[i].y + velocity*delta_t*sin(particles[i].theta);
@@ -106,15 +108,15 @@ void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted,
    */
 
   //Momentary variables for the Ecluidean distance
-  double x1, y1, x2, y2,eucli_distance;
-  int past_j;
+  double x1, y1, x2, y2, eucli_distance, best_eucli_distance;
+  //Counters and temporal variables
+  int i,j,past_j;
   //Compare each of the predicted measurements with the observation measurements
-  //std::cout << observations.size() << "   " << predicted.size() <<std::endl;
-  for(int i = 0;i < observations.size();i++){
+  for(i = 0;i < observations.size();i++){
     x1 = observations[i].x;
     y1 = observations[i].y;
-    double best_eucli_distance = 1000;
-    for(int j = 0; j < predicted.size();j++){
+    best_eucli_distance = 1000;     // Unreachable distance for the first measurement
+    for(j = 0; j < predicted.size();j++){
       x2 = predicted[j].x;
       y2 = predicted[j].y;
       //Calculate the Euclidean distance
@@ -125,6 +127,7 @@ void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted,
         past_j = j;
       }
     }
+    //Store the landmark id and erase it from the vector
     observations[i].id = predicted[past_j].id;
     predicted.erase(predicted.begin()+past_j);
   }
@@ -146,43 +149,49 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
    *   and the following is a good resource for the actual equation to implement
    *   (look at equation 3.33) http://planning.cs.uiuc.edu/node99.html
    */
-  // Momentary variables for the Homogeneous Transformation
-  LandmarkObs temp;
+  
+  // Temporal Variables
+  int id,past_k;
+  double x,y;
   double total_weight=0.0;
-  for(int i = 0; i < particles.size(); i++){
+  //Multivariate-Gaussian probability density variables
+  double multi_prob,x_cor,y_cor,mean_x,mean_y,sigma_x,sigma_y,norm,combine_prob; 
+  //Counters 
+  int i,j,k;
+  //For every particle
+  for(i = 0; i < particles.size(); i++){
     vector<LandmarkObs> observations_map_cordi;
-    //STEP 1  : Applying the homoegeneous transformation to convert the car's observations into world coordinates
-    for(int j = 0; j < observations.size(); j++){
-      temp.x = particles[i].x + (cos(particles[i].theta)*observations[j].x) - (sin(particles[i].theta)*observations[j].y);
-      temp.y = particles[i].y + (sin(particles[i].theta)*observations[j].x) + (cos(particles[i].theta)*observations[j].y);
-      observations_map_cordi.push_back(temp);   
+    //Apply the homoegeneous transformation to convert the car's observations into world coordinates
+    for(j = 0; j < observations.size(); j++){
+      id = particles[i].id;
+      x = particles[i].x + (cos(particles[i].theta)*observations[j].x) - (sin(particles[i].theta)*observations[j].y);
+      y = particles[i].y + (sin(particles[i].theta)*observations[j].x) + (cos(particles[i].theta)*observations[j].y);
+      observations_map_cordi.push_back(LandmarkObs {id,x,y});   
     } 
 
-    //STEP 2: Select the predicted landmarks that are in range of the sensor
-    double x,y;
-    int id; 
+    //Select the predicted landmarks that are in range of the sensor
     vector<LandmarkObs> prediction; 
-    for(int j = 0; j < map_landmarks.landmark_list.size(); j++){
+    for(j = 0; j < map_landmarks.landmark_list.size(); j++){
       id = map_landmarks.landmark_list[j].id_i; 
       x = map_landmarks.landmark_list[j].x_f;
       y = map_landmarks.landmark_list[j].y_f;
-      //std:: cout << particles[i].x << "   " << particles[i].y<< std::endl;
-      if((fabs(particles[i].x - x) <= sensor_range+5) && (fabs(particles[i].y - y) <= sensor_range+5)){
+      //if(dist(particles[i].x,particles[i].y,x,y) <= sensor_range){
+      if((fabs(particles[i].x - x) <= sensor_range+1) && (fabs(particles[i].y - y) <= sensor_range+1)){
         prediction.push_back(LandmarkObs {id,x,y});
       }
     }
 
-    //STEP 3: Associate the observations_map_cordi with the prediction of landmarks 
+    //Associate the observations_map_cordi with the prediction of landmarks 
     dataAssociation(prediction, observations_map_cordi);
     
-    //STEP 4: Calculate the Particle weight
+    //Calculate the Particle weight
     //Momentary variables
-    double multi_prob,x_cor,y_cor,mean_x,mean_y,sigma_x,sigma_y,norm,combine_prob = 1.0;
-    int past_k;
-    for(int j = 0; j < observations_map_cordi.size(); j++){ 
+    combine_prob = 1.0;
+    //Calculate each particles weight
+    for(j = 0; j < observations_map_cordi.size(); j++){ 
       //Search the landmark values
-      for(int k = 0; k < prediction.size(); k++){
-        //std::cout<< k <<std::endl;
+      for(k = 0; k < prediction.size(); k++){
+        //If the ids match store the counter value
         if(observations_map_cordi[j].id == prediction[k].id){
           past_k = k;
           break;
@@ -197,14 +206,16 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
       sigma_y = std_landmark[1];
       norm = 1/(2*M_PI*sigma_x*sigma_y);
       multi_prob = norm*exp(-( ((x_cor-mean_x)*(x_cor-mean_x))/(2*sigma_x*sigma_x) + ((y_cor-mean_y)*(y_cor-mean_y))/(2*sigma_y*sigma_y)  ));
+      // Combine the independent probiblities of each measurement  
       combine_prob*=multi_prob;
     }
+    //Store the particle weigth and the sum of all the particles weights
     weights[i]=combine_prob; 
     total_weight+=combine_prob;
   }
   
-  //STEP 5: Normalize the particle weigths 
-  for(int i = 0; i < weights.size(); i++){
+  // Normalize the particle weigths 
+  for(i = 0; i < weights.size(); i++){
     weights[i] = weights[i] / total_weight;
   }
 }
@@ -218,17 +229,16 @@ void ParticleFilter::resample() {
    */
   
   //Momentary variables for the Resampling wheel
-  double beta = 0.0;
-  vector<Particle> temp_particle;
+  double U,max_weight,beta = 0.0;
   int index = rand() % num_particles; //Initialization of the first index position
-  std::default_random_engine gen;   // Create the variable for Randomness
-  double U,max_weight;
-  
+  //Temporal variables
+  vector<Particle> temp_particle;
+  // Resample for each particle
   for(int i = 0; i < num_particles; i++){
-    //Implement the random number from 0 to 2*MaxWeigth for U
-    max_weight = 2.0 * *max_element(weights.begin(), weights.end());
-    U = max_weight*( (double)rand() / (double)RAND_MAX );
-    beta = beta + U ;
+    max_weight = 2.0 * *max_element(weights.begin(), weights.end()); // Max weight in the vector
+    U = max_weight*( (double)rand() / (double)RAND_MAX );   // Generates a random value (0 - 2*Max_weight)
+    beta = beta + U ; // Value to move to another index
+    //Select the next index randomly
     while(weights[index] < beta){
       beta = beta - weights[index];
       index = (index + 1) % num_particles;
@@ -237,9 +247,7 @@ void ParticleFilter::resample() {
     temp_particle.push_back(particles[index]);
   }
   //Replace the old particles with the new ones 
-  for(int j = 0; j < temp_particle.size();j++){
-    particles[j] = temp_particle[j];
-  }
+  particles = temp_particle;
 }
 
 void ParticleFilter::SetAssociations(Particle& particle, 
